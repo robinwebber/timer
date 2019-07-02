b643aa22a0df0f1714c06266be2a116410170ac5
robinwebber <robinewebber@gmail.com>
2019-07-02 19:28:35 +0000

    initial commit of timer2

---
+++ timer2.js
@@ -0,0 +1,25 @@
+// Pair coded with Hans Xu
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+
+stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    console.log(`Thanks for using me, ciao!`);
+    process.exit();
+  }
+  if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+  if (parseInt(key) >= 1 && parseInt(key) <= 9) {
+    console.log(`setting timer for ${key} seconds...`);
+    setTimeout(function() {
+
+      process.stdout.write('\x07');
+
+    }, parseInt(key) * 1000);
+  }
+  
+});
