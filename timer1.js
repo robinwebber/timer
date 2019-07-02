776ff1f05e738b4866cb3645750e0dab652a3be2
robinwebber <robinewebber@gmail.com>
2019-07-02 18:43:04 +0000

    initial commit

---
+++ timer1.js
@@ -0,0 +1,13 @@
+// Pair coded with Hans Xu
+const args = process.argv.slice(2);
+
+const timer = args => {
+  for (let numbers of args) {
+    parseFloat(numbers) > 0 && parseFloat(numbers) ? setTimeout(() => {
+      process.stdout.write('\x07');
+    }, parseFloat(numbers) * 1000) : {};
+  }
+};
+
+
+timer(args);
